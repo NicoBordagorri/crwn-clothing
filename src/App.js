b53abbfef6df7041e460f529ff01e4c76c79c418
admin <admin@localhost.localdomain>
2021-04-01 17:26:38 -0300

    added form input and custom button

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
 import Header from './components/header/header.component'
+import SignInAndSignOut from './pages/sign-in-and-sign-out/sign-in-and-sign-out.component'
 
 import {Switch,Route} from 'react-router-dom'
 
@@ -13,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path='/' component = {HomePage} />
         <Route exact path='/shop' component = {ShopPage} />
+        <Route exact path='/signin' component = {SignInAndSignOut} />
       </Switch>
     </div>
   );
